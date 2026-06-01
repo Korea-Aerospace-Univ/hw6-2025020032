09545faaaf21343f8ca18ed3d3d7cff3a17d372c
2025020032 <soomin1316@gmail.com>
2026-06-01 15:28:29 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) 
+{
+    char ch[10]={};     
+    char *p=nullptr;
+    char *q=nullptr;     
+   
+    
+    for (p=ch;p<ch+10;p++) {
+        scanf("%c",p); 
+    }
+
+  
+    char maxChar=*ch;  
+    int maxCount=0;   
+
+   
+    for (p=ch;p<ch+10;p++) {
+        int count=0;
+                                            
+        for(q=ch;q<ch+10;q++) { 
+            if (*p==*q) {  
+                count++;     
+            }
+        }
+
+        
+        if (count>maxCount) {    
+            maxCount=count; 
+            maxChar=*p
+        }
+        
+    }
+
+    printf("%c %d\n",maxChar,maxCount); 
+
+    return 0;
+}
